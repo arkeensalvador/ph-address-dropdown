7d3926833d8444156127fa20ac4af8cc8236b0e7
arkeensalvador <mitcharkeensalvador@gmail.com>
2024-05-15 12:28:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <title>Dependent Dropdown Example Philippine Address</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Philippine address dropdown, dependent address dropdown">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
